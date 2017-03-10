0c6c22b26697fda72086ae7ef3b8805b918ec9f3
huangjian <106025154@qq.com>
2017-03-10 17:36:54 +0800

    by bz
    by bz

--- d1-小爬虫/异步并发.js
+++ d1-小爬虫/异步并发.js
@@ -1,3 +1,30 @@
-/**
- * Created by huangjian on 2017/3/10.
- */
+var eventproxy= require("eventproxy");
+var superagent = require("superagent");
+var cheerio = require("cheerio");
+var url = require("url");
+var webUrl = "http://jiandan.net";
+superagent.get(webUrl).end(function(err,res){
+    if(err){
+        return console.log(err);
+    }
+    var topicUrl = [];
+    var $ = cheerio.load(res.text);
+    $(".post .indexs h2 a").each(function(index,element){
+        var $element = $(element);
+        topicUrl.push($(element).attr("href")) ;
+    })
+
+
+})
+
+
+var ep = new eventproxy();
+ep.after("ajax",topicUrl.length,function(topics){
+
+})
+topicUrl.forEach(function (item) {
+    superagent.get(item).end(function(err,res){
+        console.log('fetch'+item+"successful");
+        ep.emit("ajax",[topicUrl,res.text])
+    })
+})
