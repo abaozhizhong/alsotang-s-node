cea847fb0f4ee5de90705409c2d36c908fb6cba8
huangjian <106025154@qq.com>
2017-03-13 20:08:57 +0800

    byBz
    byBz

--- d1-小爬虫/异步并发.js
+++ d1-小爬虫/异步并发.js
@@ -1,7 +1,11 @@
 var eventproxy= require("eventproxy");
 var superagent = require("superagent");
 var cheerio = require("cheerio");
+//存链接
 var url = require("url");
+//存第一条评论
+var comments = [];
+
 var webUrl = "http://jiandan.net";
 superagent.get(webUrl).end(function(err,res){
     if(err){
@@ -14,17 +18,40 @@ superagent.get(webUrl).end(function(err,res){
         topicUrl.push($(element).attr("href")) ;
     })
 
-
+    // var eq = new eventproxy();
+    fun(topicUrl);
 })
 
+function fun(array){
+    var eq = new eventproxy();
+    eq.after("ajax",array.length,function () {
+        console.log("done");
+    });
+    for(var i in array){
+        superagent.get(array[i]).end(function (err, res) {
+            console.log("fetch"+array[i]+"successful");
+            if(err){
+                throw  err
+            }
+            var $ = cheerio.load(res.text);
 
-var ep = new eventproxy();
-ep.after("ajax",topicUrl.length,function(topics){
+                var obj = {};
+                obj.author = $(".row .author strong").html();
+                obj.text = $(".row .text p").html();
+                comments.push(obj);
+            eq.emit("ajax",res)
+        });
+    }
+}
 
-})
-topicUrl.forEach(function (item) {
-    superagent.get(item).end(function(err,res){
-        console.log('fetch'+item+"successful");
-        ep.emit("ajax",[topicUrl,res.text])
-    })
-})
+
+// var ep = new eventproxy();
+// ep.after("ajax",topicUrl.length,function(topics){
+//
+// })
+// topicUrl.forEach(function (item) {
+//     superagent.get(item).end(function(err,res){
+//         console.log('fetch'+item+"successful");
+//         ep.emit("ajax",[topicUrl,res.text])
+//     })
+// })
